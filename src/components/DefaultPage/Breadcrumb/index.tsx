2536b6f1a1cb463250c9e021ec0f8428c13553db
Michael Camacho <mikedev0431@gmail.com>
2024-08-13 21:20:22 -0700

    Updated breadcrumb with static customer steps

--- src/components/DefaultPage/Breadcrumb/index.tsx
+++ src/components/DefaultPage/Breadcrumb/index.tsx
@@ -6,23 +6,19 @@ const BreadCrumb: React.FC = (): React.ReactElement => {
   return (
     <div id="bread-crumb">
       <div className="flex flex-row max-w-[495px] h-[33px] items-center justify-between  bg-[#EDF0F8] px-[12px] rounded-[8px]">
-        <div className="text-[#A3A9C2]">Home</div>
+        <div className="text-[#3A4980] font-bold">Select Treatment</div>
         <div className="flex">
           <ArrowRight color={"#A3A9C2"} />
         </div>
-        <div className="text-[#A3A9C2]">Decoration</div>
+        <div className="text-[#A3A9C2]">Billing</div>
         <div className="flex">
           <ArrowRight color={"#A3A9C2"} />
         </div>
-        <div className="text-[#A3A9C2]">Furniture</div>
+        <div className="text-[#A3A9C2]">Payment</div>
         <div className="flex">
           <ArrowRight color={"#A3A9C2"} />
         </div>
-        <div className="text-[#A3A9C2]">Storage</div>
-        <div className="flex">
-          <ArrowRight color={"#A3A9C2"} />
-        </div>
-        <div className="text-[#3A4980] font-bold">Item</div>
+        <div className="text-[#A3A9C2]">Confirmation</div>
       </div> 
     </div>
   );
