f7d7f824eb917abba77fa503f8aa490bce73027b
Michael Camacho <mikedev0431@gmail.com>
2024-08-13 17:49:19 -0700

    updated carousel component to handle changing of slides

--- src/components/DefaultPage/Carousel/index.tsx
+++ src/components/DefaultPage/Carousel/index.tsx
@@ -1,15 +1,91 @@
 "use client";
 
+import React from "react";
 import Image from "next/image";
 
+type OnSelectSlide = () => void;
+
+const data = [
+  {
+    index: 0,
+    id: "1",
+    url: "/hero-image.jpg"
+  },
+  {
+    index: 1,
+    id: "2",
+    url: "/carousel-1.jpg"
+  },
+  {
+    index: 2,
+    id: "3",
+    url: "/carousel-2.jpg"
+  },
+  {
+    index: 3,
+    id: "4",
+    url: "/carousel-3.jpg"
+  }
+];
+
 const Carousel: React.FC = (): React.ReactElement => {
+  const [state, setState] = React.useState({
+    currentSlideIndex: 0,
+    slides: data
+  });
+
+  const onSelectSlide = (index: number): OnSelectSlide => {
+    return (): void => {
+      setState(prevState => ({
+        ...prevState,
+        currentSlideIndex: index,
+      }))
+    }
+  };
+
+
+  const renderSlides = () => state.slides.map((slide, index) => {
+    return (
+      <button key={index} onClick={onSelectSlide(index)}>
+        <div className="relative w-[115px] h-[115px] cursor-pointer">
+          <Image
+            className="rounded-2xl object-cover"
+            style={{ border: state.currentSlideIndex === index ? 1 : 0, borderStyle: state.currentSlideIndex === index ? "solid" : "none", borderColor: state.currentSlideIndex === index ? "#3A4980" : "transparent" }}
+            src={slide.url}
+            alt="hero-image"
+            priority
+            fill={true}
+          />
+        </div>
+      </button>
+    )
+  });
+
+  const onNext = (): void => {
+    if (state.currentSlideIndex === state.slides.length - 1) return;
+
+    setState(prevState => ({
+      ...prevState,
+      currentSlideIndex: prevState.currentSlideIndex + 1
+    }));
+  };
+
+  const onPrevious = (): void => {
+    if (state.currentSlideIndex === 0) return;
+
+    setState(prevState => ({
+      ...prevState,
+      currentSlideIndex: prevState.currentSlideIndex - 1
+    }));
+  };
+
   return (
     <div id="carousel" className="max-w-[587px]">
       {/* Hero Image */}
       <div className="relative w-[587px] h-[691px]">
         <Image
-          className="rounded-2xl object-cover"
-          src="/hero-image.jpg"
+          className="rounded-2xl object-cover border-[1px] border-[#EDF0F8]"
+          src={state.slides[state.currentSlideIndex].url}
           alt="hero-image"
           priority
           fill={true}
@@ -17,8 +93,9 @@ const Carousel: React.FC = (): React.ReactElement => {
       </div>
       {/* Navigation w/ preview */}
       <div className="flex flex-row pt-[40px]">
+        {/* Left Button */}
         <div className="flex pr-[9px]">
-          <button onClick={() => alert("Arrow clicked!")}>
+          <button onClick={onPrevious}>
             <Image
               src="/arrow-left.svg"
               alt="Vercel Logo"
@@ -28,46 +105,13 @@ const Carousel: React.FC = (): React.ReactElement => {
             />
           </button>
         </div>
+        {/* Preview Slides */}
         <div className="flex flex-1 flex-row justify-between">
-          <div className="relative w-[115px] h-[115px] cursor-pointer">
-            <Image
-              className="rounded-2xl object-cover"
-              src="/carousel-1.jpg"
-              alt="hero-image"
-              priority
-              fill={true}
-            />
-          </div>
-          <div className="relative w-[115px] h-[115px] cursor-pointer">
-            <Image
-              className="rounded-2xl object-cover"
-              src="/carousel-2.jpg"
-              alt="hero-image"
-              priority
-              fill={true}
-            />
-          </div>
-          <div className="relative w-[115px] h-[115px] cursor-pointer">
-            <Image
-              className="rounded-2xl object-cover"
-              src="/carousel-3.jpg"
-              alt="hero-image"
-              priority
-              fill={true}
-            />
-          </div>
-          <div className="relative w-[115px] h-[115px] cursor-pointer">
-            <Image
-              className="rounded-2xl object-cover"
-              src="/carousel-3.jpg"
-              alt="hero-image"
-              priority
-              fill={true}
-            />
-          </div>
+          {renderSlides()}
         </div>
+        {/* Right Button */}
         <div className="flex pl-[9px]">
-          <button onClick={() => alert("Arrow clicked!")}>
+          <button onClick={onNext}>
             <Image
               src="/arrow-right.svg"
               alt="Vercel Logo"
